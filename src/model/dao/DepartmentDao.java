023a0a7ed77146689a5e24156e8159855a49e1ac
Murilo <gamermurilo0@gmail.com>
2024-04-07 16:00:19 -0300

    SellerDao, DepartmentDao and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
 
+	void insert(Department dep);
+	void update(Department dep);
+	void deleteById(int id);
+	Department findById(int id);
+	List<Department> findAll();
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Seller;
+
 public interface SellerDao {
 
+	void insert(Seller dep);
+	void update(Seller dep);
+	void deleteById(int id);
+	Seller findById(int id);
+	List<Seller> findAll();
 }
