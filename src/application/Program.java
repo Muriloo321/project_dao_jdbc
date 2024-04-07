cab9c459aa66ffcd102edfe22ee7810ea241d7fa
Murilo <gamermurilo0@gmail.com>
2024-04-07 19:19:25 -0300

    FindAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,10 +18,13 @@ public class Program {
 		System.out.println(seller);
 
 		System.out.println("\n<---TEST 2: FINDBYDEPARTMENT--->");
-		Department dep = new Department(2, null);
-		List<Seller> list = selDao.findByDepartment(dep);
-		
+		List<Seller> list = selDao.findByDepartment(new Department(2, null));
 		list.forEach(System.out::println);
+		
+		System.out.println("\n<---TEST 3: FINDALL--->");
+		list = selDao.findAll();
+		list.forEach(System.out::println);
+	
 		
 	}
 
